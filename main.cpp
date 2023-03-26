4934717b09779d2fb7ab9444506d1d3d2cd63cb4
KiwiTae <adrien@movensee.com>
2023-03-26 22:34:33 +0200

    karaoke - input field text POC

--- main.cpp
+++ main.cpp
@@ -10,22 +10,44 @@ const int HEIGHT = 480;
 // GLUT window ID
 int windowID;
 
+char inputText[256] = "";
+
 // Display function
 void display()
 {
     // Clear the screen
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Draw the button
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glBegin(GL_QUADS);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(-0.5f, 0.5f);
-    glVertex2f(0.5f, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
+    // // Draw the button
+    // glColor3f(1.0f, 1.0f, 1.0f);
+    // glBegin(GL_QUADS);
+    // glVertex2f(-0.5f, -0.5f);
+    // glVertex2f(-0.5f, 0.5f);
+    // glVertex2f(0.5f, 0.5f);
+    // glVertex2f(0.5f, -0.5f);
+    // glEnd();
 
-    // Draw the text label
+    // // Draw the text label
+    // glMatrixMode(GL_PROJECTION);
+    // glPushMatrix();
+    // glLoadIdentity();
+    // gluOrtho2D(0, WIDTH, 0, HEIGHT);
+    // glMatrixMode(GL_MODELVIEW);
+    // glPushMatrix();
+    // glLoadIdentity();
+
+    // // Set the text color and position
+    // glColor3f(0.0f, 0.0f, 0.0f);
+    // glRasterPos2f(WIDTH / 2 - 40, HEIGHT / 2 + 5);
+
+    // // Draw the text
+    // const char* label = "Click me!";
+    // for (int i = 0; i < strlen(label); i++)
+    // {
+    //     glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, label[i]);
+    // }
+
+    // Draw the input field
     glMatrixMode(GL_PROJECTION);
     glPushMatrix();
     glLoadIdentity();
@@ -34,15 +56,22 @@ void display()
     glPushMatrix();
     glLoadIdentity();
 
-    // Set the text color and position
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRasterPos2f(WIDTH / 2 - 40, HEIGHT / 2 + 5);
+    // Draw the input field border
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glLineWidth(2.0f);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(WIDTH / 2 - 100, HEIGHT / 2 - 25);
+    glVertex2f(WIDTH / 2 + 100, HEIGHT / 2 - 25);
+    glVertex2f(WIDTH / 2 + 100, HEIGHT / 2 + 25);
+    glVertex2f(WIDTH / 2 - 100, HEIGHT / 2 + 25);
+    glEnd();
 
-    // Draw the text
-    const char* label = "Click me!";
-    for (int i = 0; i < strlen(label); i++)
+    // Draw the input text
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRasterPos2f(WIDTH / 2 - 90, HEIGHT / 2 - 10);
+    for (int i = 0; i < strlen(inputText); i++)
     {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, label[i]);
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, inputText[i]);
     }
 
     // Restore the modelview and projection matrices
@@ -71,6 +100,25 @@ void mouse(int button, int state, int x, int y)
     }
 }
 
+
+void keyboard(unsigned char key, int x, int y)
+{
+    // Check if the key is a printable character
+    if (key >= 32 && key <= 126 && strlen(inputText) < sizeof(inputText) - 1)
+    {
+        // Append the key to the input text
+        inputText[strlen(inputText)] = key;
+        inputText[strlen(inputText)] = '\0';
+    }
+    // Check if the key is the backspace character
+    else if (key == 8 && strlen(inputText) > 0)
+    {
+        // Remove the last character from the input text
+        inputText[strlen(inputText) - 1] = '\0';
+    }
+    glutPostRedisplay();
+}
+
 int main(int argc, char **argv)
 {
     // Initialize GLUT
@@ -84,7 +132,7 @@ int main(int argc, char **argv)
     // Register GLUT callbacks
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
-
+    glutKeyboardFunc(keyboard);
     // Start the main loop
     glutMainLoop();
 
